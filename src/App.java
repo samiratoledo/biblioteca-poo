1993566b1cbad194bd03832ab0a3d0998c9134f0
wagnermarques <wagnerdocri@gmail.com>
2026-02-24 11:08:22 -0300

    USUARIO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Usuario jose = new Usuario();
+        jose.nome = "José";
+        jose.cpf = "987.654.321-00";
+        System.out.println("Hello " + jose.nome + ", seu CPF é: " + jose.cpf);
     }
 }
---
+++ src/Usuario.java
@@ -0,0 +1,4 @@
+public class Usuario {
+    String nome = "Salomâo";
+    String cpf = "123.456.789-00";
+}
