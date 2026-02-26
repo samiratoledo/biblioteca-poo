adc7ae5107a1cdc0cf72c35140b9674a613b6d0d
Samira Toledo <samirat9ledo@gmail.com>
2026-02-26 11:57:10 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Usuario jose = new Usuario();
-        jose.nome = "José";
-        jose.cpf = "987.654.321-00";
-        System.out.println("Hello " + jose.nome + ", seu CPF é: " + jose.cpf);
+        Usuario user = new Usuario("Jorginho");
+        user.nome = "José";
+        user.cpf = "987.654.321-00";
+        System.out.println("Hello " + user.getNome());
     }
 }
